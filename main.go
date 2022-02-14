bc5e6599daef3dda3800920b84f04d74715a9f48
360ied <19516527+360ied@users.noreply.github.com>
2022-02-14 14:24:49 -0500

    allow adjusting PIN length with a cli argument

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"pingencli/pingen"
+	"strconv"
 )
 
 func main() {
-	pin, err := pingen.GeneratePIN(6)
+	length := int64(6)
+	if len(os.Args) > 1 {
+		var err error
+		length, err = strconv.ParseInt(os.Args[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	pin, err := pingen.GeneratePIN(length)
 	if err != nil {
 		panic(err)
 	}
